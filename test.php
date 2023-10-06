4a3d50d5178b27ff6965c76f78683b438b3c4bf4
Owner <kala@cougarnet.uh.edu>
2023-10-06 06:04:21 -0500

    test

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
+    <title>PHP - Helddlo, World!</title>
 </head>
 <body>
         <h1>TESTING conn</h1>
